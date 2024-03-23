8f9f5adfe820eb5c55e7314fd7e34f50d73491ae
Tiyo <eduardotiyo0@gmail.com>
2024-03-23 11:28:41 -0300

    exercicio 5 e 6

--- ed1/atividades/Atividade 3/ex1.cpp
+++ ed1/atividades/Atividade 3/ex1.cpp
@@ -51,31 +51,89 @@ bool checkName(string str){
     
 }
 
+int* arrayConcat(int* v1, int n1, int* v2, int n2){
+    int max = n1+n2;
+    int* v3 = (int*)malloc(sizeof(int)*max);
+    int i = 0;
+    for (i; i < n1; i++)
+    {
+        v3[i] = v1[i];
+    }
+    cout << i << endl;
+
+    for (int j = 0; j < n2; j++)
+    {
+        v3[i] = v2[j];
+        i++;
+    }
+    return v3;
+    
+}
+
+int* arrayMerge(int* v1, int n1, int* v2, int n2){
+    int* v3 = (int*)malloc(sizeof(int)*(n1+n2));
+    int i = 0, j = 0, z = 0;
+    while(i < n1 || j < n2){
+        if(v1[i] <= v2[j]){
+            v3[z] = v1[i];
+            i++, z++;
+            continue;
+        }
+        else{
+            v3[z] = v2[j];
+            j++, z++;
+            continue;
+        }
+    }
+    if (i < n1){
+        while (i < n1){
+            v3[z] = v1[i];
+            z++, i++;
+        }
+    }
+    if (j < n2){
+        while(j < n2){
+            v3[z] = v2[j];
+            j++, z++;
+        }
+    }
+    return v3;
+}
+
 int main(){
     
-    string str = {"o rato roeu a roupa do rei de roma"};
-    int tam = str.size();
+    // string str = {"o rato roeu a roupa do rei de roma"};
+    // int tam = str.size();
 
 
-    int pos = stringFindFirst(str, 't');
-    cout << pos << endl;
+    // int pos = stringFindFirst(str, 't');
+    // cout << pos << endl;
 
-    bool check = isLetter('s');
-    cout << check << endl;
+    // bool check = isLetter('s');
+    // cout << check << endl;
 
-    bool check = checkName("eduardo tiyo");
-    cout << check << endl;
+    // bool check = checkName("eduardo tiyo");
+    // cout << check << endl;
 
-    int* vector = createArray(5, false);
+    // int* vector = createArray(5, false);
+    // int v1[5] = {1,2,3,4,5};
+    // int v2[4] = {6,7,8,9};
+    // int* vector = arrayConcat(v1, 5, v2, 4);
     
-    for (int i = 0; i < 5; i++)
+    int v1[5] = {1,2,4,6,8};
+    int v2[4] = {3,5,9,20};
+    int* vector = arrayMerge(v1,5,v2,4);
+    for (int i = 0; i < 9; i++)
     {
         cout << vector[i] << ' ';
     }
         printf("\n");
     
     
-    delete vector;
+    // delete vector;
+    free(vector);
+
+    
 
     return 0;
 }
--- ed1/aulas/alocacaoDinamica.cpp
+++ ed1/aulas/alocacaoDinamica.cpp
@@ -11,15 +11,15 @@ int main(){
     printf("*p1: %u\n", *p1);
     printf("&p1: %u\n", &p1);
 
-    int* p2 = new int(250);
+    // int* p2 = new int(250);
     // *p2 = 50;
-    printf("\n");
+    // printf("\n");
 
-    printf("p2: %u\n", p2);
-    printf("*p2: %u\n", *p2);
-    printf("&p2: %u\n", &p2);
+    // printf("p2: %u\n", p2);
+    // printf("*p2: %u\n", *p2);
+    // printf("&p2: %u\n", &p2);
 
     free(p1);
-    delete p2;
+    // delete p2;
     return 0;
 }
