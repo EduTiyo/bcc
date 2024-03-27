2801c7b24bbc5eecf5ccffb40a1bc7f1837aa716
Tiyo <eduardotiyo0@gmail.com>
2024-03-27 16:42:34 -0300

    add pasta basico e TAD

--- ed1/atividades/Atividade 3/ex1.cpp
+++ ed1/atividades/Atividade 3/ex1.cpp
@@ -102,38 +102,36 @@ int* arrayMerge(int* v1, int n1, int* v2, int n2){
 
 int main(){
     
-    // string str = {"o rato roeu a roupa do rei de roma"};
-    // int tam = str.size();
+    string str = {"o rato roeu a roupa do rei de roma"};
+    int tam = str.size();
 
 
-    // int pos = stringFindFirst(str, 't');
-    // cout << pos << endl;
+    int pos = stringFindFirst(str, 't');
+    cout << pos << endl;
 
-    // bool check = isLetter('s');
-    // cout << check << endl;
+    bool check = isLetter('s');
+    cout << check << endl;
 
-    // bool check = checkName("eduardo tiyo");
-    // cout << check << endl;
+    bool check = checkName("eduardo tiyo");
+    cout << check << endl;
 
-    // int* vector = createArray(5, false);
-    // int v1[5] = {1,2,3,4,5};
-    // int v2[4] = {6,7,8,9};
-    // int* vector = arrayConcat(v1, 5, v2, 4);
+    int* vector = createArray(5, false);
+    int v1[5] = {1,2,3,4,5};
+    int v2[4] = {6,7,8,9};
+    int* vector = arrayConcat(v1, 5, v2, 4);
     
-    // int v1[5] = {1,2,4,6,8};
-    // int v2[4] = {3,5,9,20};
-    // int* vector = arrayMerge(v1,5,v2,4);
-    // for (int i = 0; i < 9; i++)
-    // {
-    //     cout << vector[i] << ' ';
-    // }
-    //     printf("\n");
+    int v1[5] = {1,2,4,6,8};
+    int v2[4] = {3,5,9,20};
+    int* vector = arrayMerge(v1,5,v2,4);
+    for (int i = 0; i < 9; i++)
+    {
+        cout << vector[i] << ' ';
+    }
+        printf("\n");
+    
     
     // delete vector;
-    // free(vector);
-    
-    bool res = isPalindrome(121);
-    cout << res << endl;
+    free(vector);
     
 
     return 0;
